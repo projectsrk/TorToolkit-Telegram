ce7eae42d9b2f462f313b11d0997ef00b3fd8e19
projectsrk <75125313+projectsrk@users.noreply.github.com>
2021-02-05 09:54:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
+gunicorn tortoolkit:start_server --bind 0.0.0.0:80 --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
